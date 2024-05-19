baf9ad586b3f1c344b4121fe36b5ecf7eb32e39b
Hashaam Zafar <hashaam1217@gmail.com>
2024-05-19 17:53:39 +0500

    String Formatting Verbs

--- helloworld.go
+++ helloworld.go
@@ -2,5 +2,10 @@ package main
 import ("fmt")
 
 func main() {
-  fmt.Println("Hello World!")
+    var a int = 34
+
+    fmt.Printf("%s\n", a)
+    fmt.Printf("%8s\n", a)
+    fmt.Printf("%-8s\n", a)
+    fmt.Printf("% x\n", a)
 }
