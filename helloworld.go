51a2962aa77c3d09e97dd13b65e11410e100f95a
Hashaam Zafar <hashaam1217@gmail.com>
2024-05-19 18:13:10 +0500

    Mismatch

--- helloworld.go
+++ helloworld.go
@@ -3,9 +3,11 @@ import ("fmt")
 
 func main() {
     var a int = 34
+    var array = [3]string{"Hello", "World", "!"}
 
     fmt.Printf("%s\n", a)
     fmt.Printf("%8s\n", a)
     fmt.Printf("%-8s\n", a)
     fmt.Printf("% x\n", a)
+    fmt.Println(array)
 }
